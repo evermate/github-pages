39fad53c0264468b4fd74a39256f35bb24eefda9
JYW <you@example.com>
2025-12-02 14:21:00 +0900

    chore: Configure base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/github-pages/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
